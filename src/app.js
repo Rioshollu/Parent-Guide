81e7e481ef0b4f63a5b6c19ff69cc9faaf8369f1
limjesslyn <jesslynlim.0252@gmail.com>
2022-11-08 18:03:43 +0700

    setup bootstrap

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import { Button
+ } from "react-bootstrap";
+
+ function App() {
+    return (
+        <>
+            <Button variant="primary">Uji Coba</Button>
+        </>
+    )
+ }
+
+ export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app';
+import './styles/app.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  
