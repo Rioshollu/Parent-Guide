245ae9f7339ee11861630de5a6b5dfe93281721f
Rioshollu <rioshollu21@gmail.com>
2022-11-11 15:02:57 +0700

    Test Artikel-Page

---
+++ src/pages/artikel-page.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+
+function ArtikelPage() {
+  return (
+    <>
+      <Container>
+        <Row></Row>
+      </Container>
+    </>
+  );
+}
+
+export default ArtikelPage;
