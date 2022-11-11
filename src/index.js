cc40c73e812c40273450f98d2528e5b63d158543
Rioshollu <rioshollu21@gmail.com>
2022-11-11 14:51:42 +0700

    Template Footer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app';
-import './styles/app.css';
+import './styles/style.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
