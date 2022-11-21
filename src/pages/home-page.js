e93ae8b97e488d70f1212463dc2b55f924a6e3c9
ghalibhani <ghalibpratama.gpro@gmail.com>
2022-11-21 13:56:14 +0700

    menambahkan categori rekomendasi artikel dan layanan pengaduan

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -14,34 +14,84 @@ function HomePage() {
 
       <Container>
         <Row>
-          <h1 className="title-populer">Populer Artikel</h1>
+          <h1 className="content-header">Populer</h1>
+          <hr></hr>
           <Col>
             <Card>
               <img className="artikel-image" src={artikelImage} alt="Artikel Image" />
-              <div className="p-2 m-1">
+              <div className="p-4 m-1">
                 <Card.Title>Lorem ipsum dolor sit amet</Card.Title>
-                <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum</Card.Text>
+                <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam</Card.Text>
               </div>
             </Card>
           </Col>
           <Col>
             <Card>
               <img className="artikel-image" src={artikelImage} alt="Artikel Image" />
-              <div className="p-2 m-1">
+              <div className="p-4 m-1">
                 <Card.Title>Lorem ipsum dolor sit amet</Card.Title>
-                <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum</Card.Text>
+                <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam</Card.Text>
               </div>
             </Card>
           </Col>
           <Col>
             <Card>
               <img className="artikel-image" src={artikelImage} alt="Artikel Image" />
-              <div className="p-2 m-1">
+              <div className="p-4 m-1">
                 <Card.Title>Lorem ipsum dolor sit amet</Card.Title>
-                <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum</Card.Text>
+                <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam</Card.Text>
               </div>
             </Card>
           </Col>
+        </Row>
+
+        <Row>
+          <h1 className="content-header">Rekomendasi</h1>
+          <hr></hr>
+          <Col>
+            <Card>
+              <img className="artikel-image" src={artikelImage} alt="Artikel Image" />
+              <div className="p-4 m-1">
+                <Card.Title>Lorem ipsum dolor sit amet</Card.Title>
+                <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam</Card.Text>
+              </div>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <img className="artikel-image" src={artikelImage} alt="Artikel Image" />
+              <div className="p-4 m-1">
+                <Card.Title>Lorem ipsum dolor sit amet</Card.Title>
+                <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam</Card.Text>
+              </div>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <img className="artikel-image" src={artikelImage} alt="Artikel Image" />
+              <div className="p-4 m-1">
+                <Card.Title>Lorem ipsum dolor sit amet</Card.Title>
+                <Card.Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam</Card.Text>
+              </div>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5 pb-5 content-wrapper">
+          <h1 className="content-header">Layanan Pengaduan</h1>
+          <div className="p-5 content-pengaduan">
+            <div>
+              <h4>Telepon</h4>
+              <p>110001111</p>
+            </div>
+            <div>
+              <h4>Email</h4>
+              <p>ParentGuide@gmail.com</p>
+            </div>
+            <div>
+              <h4>SMS</h4>
+              <p>2211188</p>
+            </div>
+          </div>
         </Row>
       </Container>
     </>
