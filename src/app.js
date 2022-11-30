726535b41214e6ccffaf285a9e845b69bc30b806
Rioshollu <rioshollu21@gmail.com>
2022-11-30 18:43:57 +0700

    Fitur Darkmode

--- src/app.js
+++ src/app.js
@@ -1,25 +1,47 @@
-import NavBar from './components/navbar';
-import Footer from './components/footer';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/home-page';
-import ArtikelPage from './pages/artikel-page';
-import DetailPage from './pages/detail-page';
-import AboutPage from './pages/about-page';
+import NavBar from "./components/navbar";
+import Footer from "./components/footer";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home-page";
+import ArtikelPage from "./pages/artikel-page";
+import DetailPage from "./pages/detail-page";
+import AboutPage from "./pages/about-page";
+import {useState, useMemo, useEffect} from "react";
+import ThemeContext from "./contexts/themeContent";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme((prevTheme) => {
+      let newTheme = prevTheme === "light" ? "dark" : "light";
+      localStorage.setItem("theme", newTheme);
+      return newTheme;
+    });
+  };
+  const themeContextValue = useMemo(() => {
+    return {
+      theme,
+      toggleTheme,
+    };
+  }, [theme]);
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    document.documentElement.setAttribute("data-theme", storedTheme);
+  }, [theme]);
   return (
     <>
-      <main>
-        <NavBar />
-        <Routes>
-          <Route path="/*" element={<p>Halaman Tidak Ditemukan..</p>} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="/article" element={<ArtikelPage />} />
-          <Route path="/about-us" element={<AboutPage />} />
-          <Route path="/article/detail/:id" element={<DetailPage />} />
-        </Routes>
-        <Footer />
-      </main>
+      <ThemeContext.Provider value={themeContextValue}>
+        <main data-theme={theme}>
+          <NavBar />
+          <Routes>
+            <Route path="/*" element={<p>Halaman Tidak Ditemukan..</p>} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/article" element={<ArtikelPage />} />
+            <Route path="/about-us" element={<AboutPage />} />
+            <Route path="/article/detail/:id" element={<DetailPage />} />
+          </Routes>
+          <Footer />
+        </main>
+      </ThemeContext.Provider>
     </>
   );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,32 +1,33 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import React, { useContext } from "react";
+import themeContext from "../contexts/themeContent";
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
 
 function NavBar() {
+  let { theme, toggleTheme } = useContext(themeContext);
+  theme = localStorage.getItem("theme") || "light";
   return (
-    <div className='navigation'>
-        <Navbar className="navigation-container" expand="sm">
+    <div className="navigation">
+      <Navbar className="navigation-container" expand="sm">
         <Container className="navigation-text-container">
-            <Navbar.Brand href="/" className="navigation-brand">
-            <img
-                alt="Brand Logo"
-                src="/logo-dark.svg"
-                width="50"
-                height="50"
-                className="d-inline-block align-top"
-            />{' '}
-            Parent Guide
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="/">Beranda</Nav.Link>
-                    <Nav.Link href="/article">Artikel</Nav.Link>
-                    <Nav.Link href="/about-us">Tentang Kami</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+          <Navbar.Brand href="/" className="navigation-brand">
+            <img alt="Brand Logo" src="/logo-dark.svg" width="50" height="50" className="d-inline-block align-top" /> Parent Guide
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <button type="button" className="nav-button" onClick={toggleTheme}>
+              {theme === "light" ? <MdOutlineDarkMode /> : <MdOutlineLightMode />}
+            </button>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Beranda</Nav.Link>
+              <Nav.Link href="/article">Artikel</Nav.Link>
+              <Nav.Link href="/about-us">Tentang Kami</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
-        </Navbar>
+      </Navbar>
     </div>
   );
 }
